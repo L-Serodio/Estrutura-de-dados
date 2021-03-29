3e41435ad1694c6bc2e3509eaf637fd315c3805f
Lucas_Serodio <53245868+L-Serodio@users.noreply.github.com>
2021-03-28 22:59:07 -0300

    Testes Lista Duplamente Encadeada 99.9%

--- src/lista_duplamente_encadeada/ListaDupla.java
+++ src/lista_duplamente_encadeada/ListaDupla.java
@@ -1,9 +1,9 @@
 package lista_duplamente_encadeada;
 
 public class ListaDupla<T> {
-	
+
 	private NoListaDupla<T> primeiro;
-	
+
 	public ListaDupla() {
 		primeiro = null;
 	}
@@ -11,30 +11,81 @@ public class ListaDupla<T> {
 	public NoListaDupla<T> getPrimeiro() {
 		return primeiro;
 	}
-	
+
 	public void inserir(T valor) {
-		
+		NoListaDupla<T> novo = new NoListaDupla<T>();
+		novo.setInfo(valor);
+		novo.setProximo(primeiro);
+		novo.setAnterior(null);
+		if (primeiro != null) {
+			primeiro.setAnterior(novo);
+		}
+		primeiro = novo;
 	}
-	
+
 	public NoListaDupla<T> buscar(T valor) {
-		return new NoListaDupla<T>();
+		NoListaDupla<T> p = primeiro;
+		while (p != null) {
+			if (p.getInfo().equals(valor)) {
+				return p;
+			}
+			p = p.getProximo();
+		}
+		return null;
 	}
-	
+
 	public void retirar(T valor) {
-		
+		NoListaDupla<T> p = buscar(valor);
+		if (p != null) {
+			if (primeiro.equals(p)) {
+				primeiro = p.getProximo();
+			} else {
+				p.getAnterior().setProximo(p.getProximo());
+			}
+
+			if (p.getProximo() != null) {
+				p.getProximo().setAnterior(p.getAnterior());
+			}
+		}
+
 	}
-	
+
 	public void exibirOrdemInversa() {
+		NoListaDupla<T> p = primeiro;
+		while(p.getProximo() != null) {
+			p = p.getProximo();
+		}
 		
+		while(p != primeiro) {
+			System.out.println(p + "");
+			p = p.getAnterior();
+		}
 	}
-	
+
 	public void liberar() {
+		NoListaDupla<T> p = primeiro;
+		while (p != null) {
+			NoListaDupla<T> noTemp = p.getProximo();
+			p.setProximo(null);
+			p.setAnterior(null);
+			p = noTemp;
+		}
 		
 	}
-	
+
 	@Override
 	public String toString() {
-		return "";
+		String str = "";
+		NoListaDupla<T> p = primeiro;
+		while (p != null) {
+			if (p.equals(primeiro)) {
+				str += p.getInfo().toString();
+			} else {
+				str += ", " + p.getInfo().toString();
+			}
+			p = p.getProximo();	
+		}
+		return str;
 	}
-	
+
 }
--- src/lista_duplamente_encadeada/ListaDuplaTest.java
+++ src/lista_duplamente_encadeada/ListaDuplaTest.java
@@ -6,34 +6,81 @@ import org.junit.jupiter.api.Test;
 
 class ListaDuplaTest {
 
+	ListaDupla<Integer> lista = new ListaDupla<Integer>();
+	
 	@Test
 	void testInclusao() {
-		fail("Not yet implemented");
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		assertEquals(lista.toString(), "20, 15, 10, 5");
 	}
 	
 	@Test
 	void testBuscarInicio() {
-		fail("Not yet implemented");
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		assertEquals(lista.buscar(20).getInfo(), 20);
 	}
+	
 	
 	@Test
 	void testBuscarMeio() {
-		fail("Not yet implemented");
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		assertEquals(lista.buscar(10).getInfo(), 10);
 	}
 	
 	@Test
 	void testRemoverInicio() {
-		fail("Not yet implemented");
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		lista.retirar(20);
+		assertEquals(lista.toString(), "15, 10, 5");
 	}
 	
 	@Test
 	void testRemoverMeio() {
-		fail("Not yet implemented");
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		lista.retirar(10);
+		assertEquals(lista.toString(), "20, 15, 5");
+	}
+	
+	@Test
+	void testRemoverFim() {
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		lista.retirar(5);
+		assertEquals(lista.toString(), "20, 15, 10");
 	}
 	
 	@Test
 	void testLiberar() {
-		fail("Not yet implemented");
-	}
+		lista.inserir(5);
+		lista.inserir(10);
+		lista.inserir(15);
+		lista.inserir(20);
+		lista.liberar();
+		assertEquals(lista.buscar(20).getAnterior(), null);
+		assertEquals(lista.buscar(20).getProximo(), null);
+		assertEquals(lista.buscar(15).getProximo(), null);
+		assertEquals(lista.buscar(15).getAnterior(), null);
+		assertEquals(lista.buscar(10).getProximo(), null);
+		assertEquals(lista.buscar(10).getAnterior(), null);
+		assertEquals(lista.buscar(5).getProximo(), null);
+		assertEquals(lista.buscar(5).getAnterior(), null);
+	} 
 
 }
