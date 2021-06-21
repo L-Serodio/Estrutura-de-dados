1d2cd5ac2a1fd111f6edb7514a02ee73f4111ed2
Lucas_Serodio <53245868+L-Serodio@users.noreply.github.com>
2021-06-20 22:49:54 -0300

    Ordenações e seus respectivos testes

--- src/ordenacao/OrdenacaoAbstractTest.java
+++ src/ordenacao/OrdenacaoAbstractTest.java
@@ -6,9 +6,40 @@ import org.junit.jupiter.api.Test;
 
 class OrdenacaoAbstractTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private void testar(OrdenacaoAbstract<Integer> ordenacao) {
+		Integer[] dados = new Integer[] { 70, 2, 88, 15, 90, 30 };
+		ordenacao.setInfo(dados);
+		ordenacao.ordenar();
+		
+		assertEquals(2, dados[0].intValue());
+		assertEquals(15, dados[1].intValue());
+		assertEquals(30, dados[2].intValue());
+		assertEquals(70, dados[3].intValue());
+		assertEquals(88, dados[4].intValue());
+		assertEquals(90, dados[5].intValue());
 	}
 
+	@Test
+	public void testOrdenacaoBolha() {
+		OrdenacaoAbstract<Integer> ordenacao = new OrdenacaoBolha<>();
+		testar(ordenacao);
+	}
+	
+	@Test
+	public void testOrdenacaoBolhaOtimizada() {
+		OrdenacaoAbstract<Integer> ordenacao = new OrdenacaoBolhaOtimizada<>();
+		testar(ordenacao);
+	}
+	
+	@Test
+	public void testOrdenacaoQuickSort() {
+		OrdenacaoAbstract<Integer> ordenacao = new OrdenacaoQuickSort<>();
+		testar(ordenacao);
+	}
+	
+	@Test
+	public void testOrdenacaoMergeSort() {
+		OrdenacaoAbstract<Integer> ordenacao = new OrdenacaoMergeSort<>();
+		testar(ordenacao);
+	}
 }
--- src/ordenacao/OrdenacaoBolha.java
+++ src/ordenacao/OrdenacaoBolha.java
@@ -2,4 +2,19 @@ package ordenacao;
 
 public class OrdenacaoBolha<T extends Comparable<T>> extends OrdenacaoAbstract<T> {
 
+	@Override
+	public void ordenar() {
+		int i;
+		int j;
+		T[] info = getInfo();
+		int n = info.length;
+		
+		for (i = n-1; i >= 1; i--) {
+			for (j = 0; j <= i-1; j++) {
+				if (info[j].compareTo(info[j+1]) > 0) {
+					trocar(j, j+1);
+				}
+			}
+		}
+	}
 }
